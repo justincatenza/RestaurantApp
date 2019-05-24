199333b0c54deaccb8a9732e3e6e8f12c4a67e62
Justin Catenza <justincatenza@outlook.com>
2019-05-24 12:57:43 -0500

    Service Worker V1

---
+++ js/sw.js
@@ -0,0 +1,44 @@
+const myCacheName = 'CacheV1';
+const cachesToAdd = [
+  '../css/styles.css',
+  '../data/restaurants.json',
+  '../js/dbhelper.js',
+  '../js/main.js',
+  '../js/restaurant_info.js',
+  '../index.html',
+  '../restaurant.html',
+  '../manifest.json',
+]
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(myCacheName).then(function(cache) {
+      return cache.addAll(cachesToAdd);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(myCacheName).then(function(cache) {
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != myCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
